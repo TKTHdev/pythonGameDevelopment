b7405bd856edd388cb058173bfa19268bfdf790d
高田晴成 <tkt0430@keio.jp>
2023-01-05 22:36:15 +0900

    cange

--- game.py
+++ game.py
@@ -26,7 +26,7 @@ class incomingItem:
         self.vx=pyxel.cos(angular)
         self.vy=pyxel.sin(angular)
         self.incomingSpeed=4
-        self.houkou=200
+        self.houkou=2000
 
     def move(self):
         self.x-=self.vx * self.incomingSpeed
